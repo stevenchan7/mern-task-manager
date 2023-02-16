b8f5809cc326506878f3b2327d6ec452b38406a5
Steven Chan <stevenbelvachann@gmail.com>
2023-02-16 22:30:24 +0800

    adding collection and document to db testing

--- server/app.js
+++ server/app.js
@@ -34,11 +34,30 @@ app.get('/', (req, res) => {
 });
 
 app.get('/people', (req, res) => {
-  res.status(200).json({ success: true, data: people });
+  async function find() {
+    try {
+      const task = await Task.findById('63ee2363d9cb5df154709c3e', (err, result) => {
+        if (!err) {
+          return res.status(200).json({ success: true, data: result });
+        }
+      });
+      console.log(task);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
 });
 
 app.post('/people', (req, res) => {
-  Task.create(req.body);
+  const createTask = async () => {
+    try {
+      await Task.create(req.body);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  createTask();
+
   res.status(201).json({ success: true, data: req.body });
 });
 
@@ -53,6 +72,16 @@ app.put('/people/:id', (req, res) => {
   });
   res.status(201).json({ success: true, data: newPeople });
 });
+
+// async function find() {
+//   try {
+//     const task = await Task.findById('63ee2363d9cb5df154709c3e');
+//     console.log(task);
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// }
+// find();
 
 // port
 const port = process.env.PORT || 5000;
--- server/models/task.js
+++ server/models/task.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
   taskName: String,
-  taskDue: String,
-  completed: Boolean,
+  taskDue: String, // Date but for now let it be string
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const Task = mongoose.model('Task', taskSchema);
